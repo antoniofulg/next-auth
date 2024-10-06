4a5818ee2009656ec5bfc1bb853fe08a1e92fc87
Antonio Fulgencio <antoniofulg@gmail.com>
2024-10-05 22:02:45 -0300

    Add register user feat

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server"
 
+import { hash } from "bcrypt"
+
+import { db } from "@/lib/db"
 import { RegisterSchema } from "@/schemas"
 
 import * as z from "zod"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedFields = RegisterSchema.safeParse(values)
@@ -10,6 +14,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	if (!validatedFields.success) {
 		return { error: "Invalid credentials" }
 	}
+
+	const { email, password, name } = validatedFields.data
+	const hashedPassword = await hash(password, 10)
+
+	const existingUser = await getUserByEmail(email)
+
+	if (existingUser) {
+		return { error: "Email already in use" }
+	}
+
+	await db.user.create({
+		data: {
+			email,
+			password: hashedPassword,
+			name,
+		},
+	})
+
+	// TODO: Send verification token email
 
 	return { success: "Email sent" }
 }
